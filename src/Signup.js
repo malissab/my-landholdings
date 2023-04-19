3dc240f502225ade052c01c8c66875bc4641a9f4
malissab <malissambishop7@gmail.com>
2023-04-19 16:44:39 -0700

    signup page created

--- src/Signup.js
+++ src/Signup.js
@@ -1,11 +1,39 @@
 import React, { useState } from 'react'
 
 function Signup() {
-    
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
+    const handleEmail = (e) => {
+        setEmail(e.target.value)
+    }
+    const handlePassword = (e) => {
+        setPassword(e.target.value)
+    }
+
 
 
   return (
-    <div>Signup</div>
+    <div>
+        Signup
+        <form onSubmit={handleSubmit}>
+            <label>
+                Email: 
+                <input type="email" value={email} onChange={handleEmail} />
+            </label>
+            <br />
+            <label>
+                Password: 
+                <input type="password" value={password} onChange={handlePassword} />
+            </label>
+            <br />
+            <button type="submit">Sign Up</button>
+        </form>
+
+    </div>
   )
 }
 
